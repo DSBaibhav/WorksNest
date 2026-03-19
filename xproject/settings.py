75f94938d6227c4488fcc342aec513e23cf9d581
DSBaibhav <baibhavsingh473@gmail.com>
2026-03-19 20:37:58 +0530

    Fix Job detail page,add status banners and minor bug fixes

--- xproject/settings.py
+++ xproject/settings.py
@@ -38,10 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app1',
-    'crispy_forms',
-    'crispy_bootstrap5',
-    'blog',
     'core',
 ]
 
